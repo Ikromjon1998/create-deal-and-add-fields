59ba8bb77e7a509a000309ad1d5e86208328dc5d
Ochilov <57859849+Ikromjon1998@users.noreply.github.com>
2023-07-23 19:24:37 +0200

    added cookie

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-const pipedrive = require("pipedrive");
+
 const express = require("express");
 // Load environment variables. Make sure you fill the .env file
 require("dotenv").config();
-
 const app = express();
-let token;
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+
+app.use(cookieParser());
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1']
+}));
+const pipedrive = require("pipedrive");
+const apiClient = new pipedrive.ApiClient();
+let oauth2 = apiClient.authentications.oauth2;
+oauth2.clientId = process.env.CLIENT_ID;
+oauth2.clientSecret = process.env.CLIENT_SECRET;
+oauth2.redirectUri = `https://${process.env.PROJECT_DOMAIN}.onrender.com/callback`;
+
 app.use(express.json());
+
 
 app.use('/', express.static("public"));
 
@@ -31,21 +45,32 @@ app.post("/", function (req, res) {
     }
 });
 
+app.get('/alldeals', async (req, res) => {
+    if (req.session.accessToken !== null && req.session.accessToken !== undefined) {
+        // token is already set in the session
+        // now make API calls as required
+        // client will automatically refresh the token when it expires and call the token update callback
+        const api = new pipedrive.DealsApi(apiClient);
+        const deals = await api.getDeals();
+
+        res.send(deals);
+    } else {
+        const authUrl = apiClient.buildAuthorizationUrl();;
+
+        res.redirect(authUrl);
+    }
+});
+
 app.get("/callback", async function (req, res) {
-    const apiClient = new pipedrive.ApiClient();
-
-    let oauth2 = apiClient.authentications.oauth2;
-    oauth2.clientId = process.env.CLIENT_ID;
-    oauth2.clientSecret = process.env.CLIENT_SECRET;
-    oauth2.redirectUri = `https://${process.env.PROJECT_DOMAIN}.onrender.com/callback`;
-
-    const authUrl = apiClient.buildAuthorizationUrl();
     if (req.query.code) {
         try {
-            token = await apiClient.authorize(req.query.code);
-            console.log(token);
-            console.log("Successful Auth ✅");
-            return res.status(200).redirect("/");
+            const promise = await apiClient.authorize(req.query.code);
+            promise.then(() => {
+                req.session.accessToken = apiClient.authentications.oauth2.accessToken;
+                return res.redirect('/');
+            }, (exception) => {
+                // error occurred, exception will be of type src/exceptions/OAuthProviderException
+            });
         } catch (error) {
             console.error("Wob, Wob, Wobb 🙁");
             console.error(error);
